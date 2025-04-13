32e9f914a59f7332e644953e9a1b3eaf22c76c78
TerrellAW <terraw1999@proton.me>
2025-04-12 18:19:13 -0600

    Added ILevel interface

---
+++ Levels/ILevel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StickManWallJump;
+
+internal interface ILevel
+{
+    /// <summary>
+    /// 
+    /// Levels will have their own variables, such as ground/platform friction, so some levels are slippery and some aren't.
+    /// Levels will also have their own textures, such as the background, platforms, and walls.
+    /// Backgrounds will be audio visualizers that react to that level's music. Levels will have different music.
+    /// 
+    /// Below are example functions, some might be fleshed out, some might be removed.
+    /// </summary>
+    void LoadLevel();
+    void UnloadLevel();
+    void UpdateLevel(float deltaTime);
+    void DrawLevel();
+    void ResetLevel();
+    bool IsLevelComplete();
+    bool IsLevelFailed();
+    void RestartLevel();
+}
