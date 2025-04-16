4a91c012ab00c442dcbbc51070bc46354809e639
TerrellAW <terraw1999@proton.me>
2025-04-16 13:46:39 -0600

    Converted ILevel to abstract class

--- Levels/Level.cs
+++ Levels/Level.cs
@@ -6,22 +6,13 @@ using System.Threading.Tasks;
 
 namespace StickManWallJump;
 
-internal interface ILevel
+internal abstract class Level
 {
     /// <summary>
     /// 
     /// Levels will have their own variables, such as ground/platform friction, so some levels are slippery and some aren't.
     /// Levels will also have their own textures, such as the background, platforms, and walls.
     /// Backgrounds will be audio visualizers that react to that level's music. Levels will have different music.
-    /// 
-    /// Below are example functions, some might be fleshed out, some might be removed.
+    ///
     /// </summary>
-    void LoadLevel();
-    void UnloadLevel();
-    void UpdateLevel(float deltaTime);
-    void DrawLevel();
-    void ResetLevel();
-    bool IsLevelComplete();
-    bool IsLevelFailed();
-    void RestartLevel();
 }
