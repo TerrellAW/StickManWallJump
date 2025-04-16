28b98e6b5d5878e143f3ae9d36ae2e09416cdfb5
TerrellAW <terraw1999@proton.me>
2025-04-16 13:41:02 -0600

    LevelObject abstract class

--- Levels/Level_Objects/LevelObject.cs
+++ Levels/Level_Objects/LevelObject.cs
@@ -8,4 +8,18 @@ namespace StickManWallJump;
 
 internal abstract class LevelObject
 {
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public bool IsSolid { get; set; }
+    public bool IsVisible { get; set; }
+    public bool IsActive { get; set; }
+
+    public LevelObject(int id, string name, bool isSolid, bool isVisible, bool isActive)
+    {
+        ID = id;
+        Name = name;
+        IsSolid = isSolid;
+        IsVisible = isVisible;
+        IsActive = isActive;
+    }
 }
