9e0fa2ffca1012a88ee350450d1ee85477984288
TerrellAW <terraw1999@proton.me>
2025-04-16 21:56:05 -0600

    Fixed LevelObject constructors

--- Levels/Level_Objects/LevelObject.cs
+++ Levels/Level_Objects/LevelObject.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace StickManWallJump;
 
 internal abstract class LevelObject
 {
+    // Texture
+    public abstract Texture2D Texture { get; set; }
+
+    // Properties
     public int ID { get; set; }
     public float X { get; set; }
     public float Y { get; set; }
     public float Width { get; set; }
     public float Height { get; set; }
 
+    // Constructor
     public LevelObject(int id, float x, float y, float width, float height)
     {
         ID = id;
--- Levels/Level_Objects/Platform.cs
+++ Levels/Level_Objects/Platform.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace StickManWallJump;
 
 internal class Platform : LevelObject
 {
+    // Texture
+    public override Texture2D Texture { get; set; }
+
+    // Constructor
     public Platform(int id, float x, float y, float width, float height) : base(id, x, y, width, height)
     {
+        this.ID = id;
+        this.X = x;
+        this.Y = y;
+        this.Width = width;
+        this.Height = height;
     }
 }
