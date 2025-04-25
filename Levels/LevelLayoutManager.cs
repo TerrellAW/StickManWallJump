c91f87a571f4134a461a4f4ec6117a88fa6e82b1
TerrellAW <terraw1999@proton.me>
2025-04-25 16:42:32 -0600

    Plan: LevelLayoutManager

--- Levels/LevelLayoutManager.cs
+++ Levels/LevelLayoutManager.cs
@@ -2,9 +2,12 @@ using System.Collections.Generic;
 
 namespace StickManWallJump;
 
-public class LevelLayout
+public class LevelLayoutManager // TODO: Make separate LevelLayout class
 {
     private Level _currentLevel;
+
+    // Store level layout data here, each level will be identified with levelName string
+    // Function will be able to identify which level layout it needs and run the commands to create it
 
     // Function that accepts levelName string and _currentLevel from LevelManager
     // Will run all the necessary AddPlatform and AddWall commands to generate Level
