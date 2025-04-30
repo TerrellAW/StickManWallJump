08f09a8feecfe8c03e95257e95380b8bd8d74f27
TerrellAW <terraw1999@proton.me>
2025-04-29 19:13:53 -0600

    Feature: Debug mode

--- Program.cs
+++ Program.cs
@@ -1,9 +1,28 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
 using StickManWallJump;
 
-// Create level manager
-LevelManager levelManager = new LevelManager();
+public static class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        // Parse command line arguments for debug mode
+        bool debugMode = false;
 
-// Add conditions for switching levels, menus, etc.
-using var game = new Engine(levelManager, "Level1");
-game.Run();
+        foreach (string arg in args)
+        {
+            if (arg.ToLower() == "--debug" || arg.ToLower() == "-d")
+            {
+                debugMode = true;
+                break;
+            }
+        }
+
+        // Create level manager
+        LevelManager levelManager = new LevelManager();
+
+        // Pass debug flag to Engine constructor
+        using var game = new Engine(levelManager, "Level1", debugMode);
+        game.Run();
+    }
+}
