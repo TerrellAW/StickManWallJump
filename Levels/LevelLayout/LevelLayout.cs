9a758a787ba71bd78e9178b2de438eca317f28e3
TerrellAW <terraw1999@proton.me>
2025-06-03 18:42:41 -0600

    Plan: CRUD methods

--- Levels/LevelLayout/LevelLayout.cs
+++ Levels/LevelLayout/LevelLayout.cs
@@ -1,2 +1,17 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace StickManWallJump.Levels.LevelLayout;
+
 // TODO: Class which contains all info needed for createLevel function in LevelManager
-// LevelLayout will contain array of game objects and their locations
+// LevelLayout will contain array of level objects and their locations
+internal class LevelLayout
+{
+    // TODO: key-value pair arrays with platform index and Vector2 position
+    private Dictionary<int, Vector2> _platforms = new Dictionary<int, Vector2>();
+
+    // TODO: key-value pair arrays with wall index and Vector2 position
+    private Dictionary<int, Vector2> _walls = new Dictionary<int, Vector2>();
+
+    // TODO: Methods to modify _platforms and _walls, full CRUD capabilities for level editor
+}
--- Levels/LevelLayout/LevelLayoutManager.cs
+++ Levels/LevelLayout/LevelLayoutManager.cs
@@ -1,2 +1,15 @@
+using System.Collections.Generic;
+
+using StickManWallJump.Levels.LevelLayout;
+
+namespace StickManWallJump;
+
 // TODO: Take index from LevelManager and use it to find the correct LevelLayout
 // LevelLayout will contain array of game objects and their locations
+internal class LevelLayoutManager
+{
+    // TODO: List of LevelLayouts and methods to access and utilize them
+    private List<LevelLayout> _levelLayouts = new List<LevelLayout>();
+
+    // TODO: Methods to modify _levelLayouts, full CRUD capabilities for level editor
+}
